02d8ae320b55eea1cdee3f5eddf1a04b5605d02c
Tirth Patel <tirthpatel7498@gmail.com>
2020-08-24 13:29:49 -0400

    Update to life.lua

--- Lua/life.lua
+++ Lua/life.lua
@@ -37,42 +37,46 @@ do
 
     function Life:print_matrix()
 		local table = self.table
-		for i = 1, self.r+2 do
-			io.write('*')
-		end
+		-- for i = 1, self.r+2 do
+		-- 	io.write('*')
+		-- end
 		print('')
 		for i = 1, self.r do
-			io.write('*')
+			--io.write('*')
 			for j = 1, self.c do
 				if table[i][j] == 0 then
-					io.write('.')
+					io.write('. ')
 				else 
-					io.write('O')
+					io.write('O ')
 				end
 			end
-			print('*')
+			--print('*')
+			io.write('\n')
 		end	
-		for i = 1, self.r+2 do
-			io.write('*')
-		end
+		-- for i = 1, self.r+2 do
+		-- 	io.write('*')
+		-- end
 		print('')
     end
     
     function Life:moveToNewGen()
         --firstly we need to create a copy of the previous table because any changes to the original table may affect the individual cell outcomes...
-        local table2 = self.table
+		local table2 = self.table
+		
+		--Lets make changes to table2 and then compy the table 2 into main table
         for i = 1, self.r do
 			for j = 1, self.c do
-				local s = 0
+				local numOfNeighbors = 0
 				for p = i-1,i+1 do
 					for q = j-1,j+1 do
 						if p > 0 and p <= self.r and q > 0 and q <= self.c then
-							s = s + self.table[p][q]
+							numOfNeighbors = numOfNeighbors + self.table[p][q]
 						end
 					end
 				end
-				s = s - self.table[i][j]
-				if s == 3 or (s+self.table[i][j]) == 3 then
+				-- remove the current cell from numOfNeighbors
+				numOfNeighbors = numOfNeighbors - self.table[i][j]
+				if numOfNeighbors == 3 or (numOfNeighbors+self.table[i][j]) == 3 then
 					table2[i][j] = 1
 				else
 					table2[i][j] = 0
@@ -85,29 +89,69 @@ do
 
 
 end
-local life = Life:createTable(10,10)
 
-life:setLife(3,3)
-life:setLife(1,2)
-life:setLife(1,4)
-life:setLife(5,5)
-life:setLife(2,3)
-life:setLife(3,1)
-life:setLife(3,2)
-life:setLife(5,2)
-life:setLife(6,2)
-life:setLife(5,6)
-life:setLife(6,6)
-life:setLife(7,7)
+local life = Life:createTable(17,17)
+
+life:setLife(2, 4);
+life:setLife(2, 5);
+life:setLife(2, 6);
+life:setLife(2, 10);
+life:setLife(2, 11);
+life:setLife(2, 12);
+life:setLife(4, 2);
+life:setLife(4, 7);
+life:setLife(4, 9);
+life:setLife(4, 14);
+life:setLife(5, 2);
+life:setLife(5, 7);
+life:setLife(5, 9);
+life:setLife(5, 14);
+life:setLife(6, 2);
+life:setLife(6, 7);
+life:setLife(6, 9);
+life:setLife(6, 14);
+life:setLife(7, 4);
+life:setLife(7, 5);
+life:setLife(7, 6);
+life:setLife(7, 10);
+life:setLife(7, 11);
+life:setLife(7, 12);
+life:setLife(9, 4);
+life:setLife(9, 5);
+life:setLife(9, 6);
+life:setLife(9, 10);
+life:setLife(9, 11);
+life:setLife(9, 12);
+life:setLife(10, 2);
+life:setLife(10, 7);
+life:setLife(10, 9);
+life:setLife(10, 14);
+life:setLife(11, 2);
+life:setLife(11, 7);
+life:setLife(11, 9);
+life:setLife(11, 14);
+life:setLife(12, 2);
+life:setLife(12, 7);
+life:setLife(12, 9);
+life:setLife(12, 14);
+life:setLife(14, 4);
+life:setLife(14, 5);
+life:setLife(14, 6);
+life:setLife(14, 10);
+life:setLife(14, 11);
+life:setLife(14, 12);
 
 
 
-life:print_matrix(10,10)
-gen = 1
-while(gen < 50) do
-    --io.write(gen)
-    life:moveToNewGen()
+gen = 0
+io.write("Generation: ", gen, "\n")
+life:print_matrix()
+gen = gen+1
+while(gen <= 30) do
+    
+	life:moveToNewGen()
+	io.write("Generation: ", gen, "\n")
+	life:print_matrix()
+	io.write("\n")
     gen = gen+1
 end
-
-life:print_matrix(10,10)
