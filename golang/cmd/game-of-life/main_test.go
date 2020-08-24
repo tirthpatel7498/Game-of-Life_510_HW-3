c31fed21c14ee32e7834965529b62cf2c4817d3c
Dan Goslen <dgoslen@bandwidth.com>
2020-08-24 17:45:17 -0400

    Adding two simple test for neighbors

---
+++ golang/cmd/game-of-life/main_test.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// 0 0 0 0 0
+// 0 1 0 0 0
+// 0 0 0 0 0
+// 0 0 0 0 0
+// 0 0 0 0 0
+func TestZeroNeighbors(t *testing.T) {
+	rows := 3
+	cols := 3
+
+	generation := make([]int, (rows+2)*(cols+2))
+
+	generation[7] = 1
+	neighbors := neighbors(cols, generation, 7)
+
+	assert.Equal(t, neighbors, 0, "The first cell has no alive neighbors")
+}
+
+// 0 0 0 0 0
+// 0 1 0 0 0
+// 0 1 0 0 0
+// 0 0 0 0 0
+// 0 0 0 0 0
+func TestOneNeighbor(t *testing.T) {
+	rows := 3
+	cols := 3
+
+	generation := make([]int, (rows+2)*(cols+2))
+
+	generation[7] = 1
+	generation[7+5] = 1
+	neighbors := neighbors(cols, generation, 7)
+
+	assert.Equal(t, neighbors, 1, "The first cell has no alive neighbors")
+}
