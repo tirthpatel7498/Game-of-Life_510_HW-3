1f84f495ec28ec4ae55481cde717f7bc868a2378
harshkachhadia <harsh.lions@gmail.com>
2020-08-24 19:09:53 -0400

    Updates test for dart

--- Dart/gol_test.dart
+++ Dart/gol_test.dart
@@ -1,8 +1,4 @@
-import 'package:test/test.dart';
 
 void main() {
-  test('String.split() splits the string on the delimiter', () {
-    var string = 'game,of,life';
-    expect(string.split(','), equals(['game', 'of', 'life']));
-  });
+print("Testing the Game of Life");
 }
